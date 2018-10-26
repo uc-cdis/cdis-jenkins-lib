e5a3133cd0f00db9a0ed1bd4ca7811c2014fd9a9
Ted Summer <ted.summer2@gmail.com>
2018-10-26 16:28:58 -0500

    debug(microservice): testing config

--- vars/selectAndLockNamespace.groovy
+++ vars/selectAndLockNamespace.groovy
@@ -4,13 +4,13 @@ def call(config) {
   script {
     // def namespaces = config.namespaceChoices.replaceAll("\\s","").split(',')
     List<String> namespaces = config.namespaceChoices
-    int randNum = new Random().nextInt(size(namespaces));
+    int randNum = new Random().nextInt(namespaces.size());
     uid = env.service+"-"+"$env.GIT_BRANCH".replaceAll("/", "_")+"-"+env.BUILD_NUMBER
     int lockStatus = 1;
 
     // try to find an unlocked namespace
-    for (int i=0; i < size(namespaces) && lockStatus != 0; ++i) {
-      randNum = (randNum + i) % size(namespaces);
+    for (int i=0; i < namespaces.size() && lockStatus != 0; ++i) {
+      randNum = (randNum + i) % namespaces.size();
       env.KUBECTL_NAMESPACE = namespaces.get(randNum)
       println "selected namespace $env.KUBECTL_NAMESPACE on executor $env.EXECUTOR_NUMBER"
       println "attempting to lock namespace $env.KUBECTL_NAMESPACE with a wait time of 1 minutes"
