c7ee6ca561a51b124a677cfd82216941caa8d962
Ted Summer <ted.summer2@gmail.com>
2018-10-26 16:31:36 -0500

    debug(microservice): testing config

--- vars/klockNamespace.groovy
+++ vars/klockNamespace.groovy
@@ -1,8 +1,8 @@
 #!groovy
 
 // Attempts to lock KUBECTL_NAMESPACE
-def call(method, uid) {
+def call(Map params) {
   withEnv(['GEN3_NOPROXY=true', "GEN3_HOME=$env.WORKSPACE/cloud-automation"]) {
-    return sh( script: "bash cloud-automation/gen3/bin/klock.sh "+method+" jenkins "+uid+" 3600 -w 60", returnStatus: true)
+    return sh( script: "bash cloud-automation/gen3/bin/klock.sh "+params.method+" jenkins "+params.uid+" 3600 -w 60", returnStatus: true)
   }
 }
