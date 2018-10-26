e99d4a7d086514fe1a0682456c8f4c946ccc9a01
Ted Summer <ted.summer2@gmail.com>
2018-10-26 16:27:08 -0500

    debug(microservice): testing config

--- vars/selectAndLockNamespace.groovy
+++ vars/selectAndLockNamespace.groovy
@@ -2,15 +2,16 @@
 
 def call(config) {
   script {
-    def namespaces = config.namespaceChoices.replaceAll("\\s","").split(',')
-    int randNum = new Random().nextInt(namespaces.length);
+    // def namespaces = config.namespaceChoices.replaceAll("\\s","").split(',')
+    List<String> namespaces = config.namespaceChoices
+    int randNum = new Random().nextInt(size(namespaces));
     uid = env.service+"-"+"$env.GIT_BRANCH".replaceAll("/", "_")+"-"+env.BUILD_NUMBER
     int lockStatus = 1;
 
     // try to find an unlocked namespace
-    for (int i=0; i < namespaces.length && lockStatus != 0; ++i) {
-      randNum = (randNum + i) % namespaces.length;
-      env.KUBECTL_NAMESPACE = namespaces[randNum]
+    for (int i=0; i < size(namespaces) && lockStatus != 0; ++i) {
+      randNum = (randNum + i) % size(namespaces);
+      env.KUBECTL_NAMESPACE = namespaces.get(randNum)
       println "selected namespace $env.KUBECTL_NAMESPACE on executor $env.EXECUTOR_NUMBER"
       println "attempting to lock namespace $env.KUBECTL_NAMESPACE with a wait time of 1 minutes"
       withEnv(['GEN3_NOPROXY=true', "GEN3_HOME=$env.WORKSPACE/cloud-automation"]) {
