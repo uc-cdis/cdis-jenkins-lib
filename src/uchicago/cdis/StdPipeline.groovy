c3660903a1b3d8c46d0fed4f3452ae6e5a58100f
Ted Summer <ted.summer2@gmail.com>
2018-10-31 16:41:09 -0500

    debug(classes): try composition with kube

--- src/uchicago/cdis/StdPipeline.groovy
+++ src/uchicago/cdis/StdPipeline.groovy
@@ -3,29 +3,29 @@ package uchicago.cdis;
 
 class StdPipeline implements Serializable {
   def myVariable
-}
 
-def execute(Map pipelineDefinition) {
+  def execute(Map pipelineDefinition) {
 
-  node {
+    node {
 
-    stage('Initialize') {
-      // checkout scm
-      echo 'Loading pipeline definition'
-      // Yaml parser = new Yaml()
-      // Map pipelineDefinition = parser.load(new File(pwd() + '/pipeline.yml').text)
-    }
+      stage('Initialize') {
+        // checkout scm
+        echo 'Loading pipeline definition'
+        // Yaml parser = new Yaml()
+        // Map pipelineDefinition = parser.load(new File(pwd() + '/pipeline.yml').text)
+      }
 
-    switch('hello world') {
-      case 'hello world':
-        // Instantiate and execute a Python pipeline
-        new MicroservicePipeline(pipelineDefinition).executePipeline()
-      case 'nodejs':
-        // Instantiate and execute a NodeJS pipeline
-        // new nodeJSPipeline(pipelineDefinition).executePipeline()
-        echo "Hello world"
+      switch('hello world') {
+        case 'hello world':
+          // Instantiate and execute a Python pipeline
+          new MicroservicePipeline(pipelineDefinition).executePipeline()
+        case 'nodejs':
+          // Instantiate and execute a NodeJS pipeline
+          // new nodeJSPipeline(pipelineDefinition).executePipeline()
+          echo "Hello world"
+      }
+
     }
 
   }
-
 }
