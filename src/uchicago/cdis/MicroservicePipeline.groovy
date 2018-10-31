6a40fc7f6b024cca2941096574b67eb552ead2de
Ted Summer <ted.summer2@gmail.com>
2018-10-31 15:54:27 -0500

    debug(classes): try composition with kube

--- src/uchicago/cdis/MicroservicePipeline.groovy
+++ src/uchicago/cdis/MicroservicePipeline.groovy
@@ -1,25 +1,29 @@
 #!/usr/bin/groovy
 package uchicago.cdis;
+class MicroservicePipeline implements Serializable {
+  Kube kube
 
-MicroservicePipeline(pipelineDefinition) {
-  // Create a globally accessible variable that makes
-  // the YAML pipeline definition available to all scripts
-  // pd = pipelineDefinition
-  @Delegate
-  Kube kube = new Kube()
-}
+  MicroservicePipeline(pipelineDefinition) {
+    // Create a globally accessible variable that makes
+    // the YAML pipeline definition available to all scripts
+    // pd = pipelineDefinition
+    @Delegate
+    Kube kube = new Kube()
+  }
 
-def execute() {
-  node {
-    stage('Run Tests') {
-      echo "RUNNING THE TEST"
-      this.kube.deploy()
-    }
+  def execute() {
+    node {
+      stage('Run Tests') {
+        echo "RUNNING THE TEST"
+        kube.deploy()
+      }
 
-    if (signalSlack) {
-      stage('Deploy') {
-        sh "path/to/a/deploy/bash/script.sh ${pd.deploymentEnvironment}"
+      if (signalSlack) {
+        stage('Deploy') {
+          sh "path/to/a/deploy/bash/script.sh ${pd.deploymentEnvironment}"
+        }
       }
     }
   }
 }
+
