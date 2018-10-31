89aa50825fceba5651bcdc4d9097ddec0431cbbb
Ted Summer <ted.summer2@gmail.com>
2018-10-31 15:32:47 -0500

    debug(classes): try composition with kube

---
+++ src/uchicago/cdis/MicroservicePipeline.groovy
@@ -0,0 +1,25 @@
+#!/usr/bin/groovy
+package uchicago.cdis;
+
+MicroservicePipeline(pipelineDefinition) {
+  // Create a globally accessible variable that makes
+  // the YAML pipeline definition available to all scripts
+  // pd = pipelineDefinition
+  @Delegate
+  Kube kube = new Kube()
+}
+
+def execute() {
+  node {
+    stage('Run Tests') {
+      echo "RUNNING THE TEST"
+      kube.deploy()
+    }
+
+    if (signalSlack) {
+      stage('Deploy') {
+        sh "path/to/a/deploy/bash/script.sh ${pd.deploymentEnvironment}"
+      }
+    }
+  }
+}
