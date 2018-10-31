ad43654cb36e0164741ac1715c96c375875997b0
Ted Summer <ted.summer2@gmail.com>
2018-10-31 10:14:05 -0500

    refactor(testPipeline): make generic entry point for starting a pipeline

---
+++ vars/startPipeline.groovy
@@ -0,0 +1,18 @@
+#!groovy
+
+// This is the main entry point for all testing
+def call(body) {
+  // evaluate the body block, and collect configuration into the object
+  def config = [:]
+  body.resolveStrategy = Closure.DELEGATE_FIRST
+  body.delegate = config
+  body()
+
+  // call the pipeline indicated in config
+  if (config.pipeline == 'microservice') {
+    microservicePipeline(config)
+  }
+  else if (config.pipeline == 'gitops') {
+    gitopsPipeline(config)
+  }
+}
