dff55cd7484ca101b18dd49645f027a005e6deb4
Reuben Pasquini <reuben@frickjack.com>
2018-11-13 11:08:27 -0600

    fix(pipeline): typo

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -170,7 +170,6 @@ def call(Map config) {
           dir('gen3-qa') {
             withEnv(['GEN3_NOPROXY=true', "vpc_name=$env.KUBECTL_NAMESPACE", "GEN3_HOME=$env.WORKSPACE/cloud-automation", "NAMESPACE=$env.KUBECTL_NAMESPACE", "TEST_DATA_PATH=$env.WORKSPACE/testData/"]) {
               sh "bash ./jenkins-simulate-data.sh $env.KUBECTL_NAMESPACE"
-              sh "bash ./run-tests.sh $env.KUBECTL_NAMESPACE"
             }
           }
         }
