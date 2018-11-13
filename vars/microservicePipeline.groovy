6a03d0e72e3ea9ba4e3a8d7687045a1c46f4f3a2
Reuben Pasquini <reuben@frickjack.com>
2018-11-13 09:26:30 -0600

    chore(pipeline): split out generate data stage

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -164,12 +164,22 @@ def call(Map config) {
           }
         }
       }
-      stage('RunTests') {
+      stage('GenerateTestData') {
+        // Run the data simulator
         steps {
           dir('gen3-qa') {
             withEnv(['GEN3_NOPROXY=true', "vpc_name=$env.KUBECTL_NAMESPACE", "GEN3_HOME=$env.WORKSPACE/cloud-automation", "NAMESPACE=$env.KUBECTL_NAMESPACE", "TEST_DATA_PATH=$env.WORKSPACE/testData/"]) {
               sh "bash ./jenkins-simulate-data.sh $env.KUBECTL_NAMESPACE"
               sh "bash ./run-tests.sh $env.KUBECTL_NAMESPACE"
+            }
+          }
+        }
+      }
+      stage('RunTests') {
+        steps {
+          dir('gen3-qa') {
+            withEnv(['GEN3_NOPROXY=true', "vpc_name=$env.KUBECTL_NAMESPACE", "GEN3_HOME=$env.WORKSPACE/cloud-automation", "NAMESPACE=$env.KUBECTL_NAMESPACE", "TEST_DATA_PATH=$env.WORKSPACE/testData/"]) {
+              sh "bash ./run-tests.sh $env.KUBECTL_NAMESPACE --service=$env.service"
             }
           }
         }
