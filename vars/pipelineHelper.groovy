1dd31371a933c5965957251031befa435f39d595
Ted Summer <ted.summer2@gmail.com>
2018-11-30 13:49:34 -0600

    test(pipe): var tests

--- vars/pipelineHelper.groovy
+++ vars/pipelineHelper.groovy
@@ -10,13 +10,13 @@ def create(Map config) {
 
 def init(Map config) {
   gitVars = checkout(scm)
-  if (config & !config.GIT_BRANCH) {
+  if (config & (!config.GIT_BRANCH)) {
     config.GIT_BRANCH = gitVars.GIT_BRANCH
     config.GIT_COMMIT = gitVars.GIT_COMMIT
   }
   config.BRANCH_FORMATTED = "${config.GIT_BRANCH}".replaceAll("/", "_")
 
-  if (config && !config.JOB_NAME) {
+  if (config && (!config.JOB_NAME)) {
     config.JOB_NAME = "$env.JOB_NAME".split('/')[1]
   }
   config.UID = "${config.JOB_NAME}-${config.BRANCH_FORMATTED}-${env.BUILD_NUMBER}"
