635921b7c327a9435b2c2488deabe1bca9a06892
Reuben Pasquini <reuben@frickjack.com>
2018-12-21 12:38:26 -0600

    test(jenkins-genomel): force genomel as a test

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -140,14 +140,14 @@ def call(Map config) {
         steps {
           script {
             String[] namespaces = ['jenkins-brain', 'jenkins-niaid', 'jenkins-dcp', 'jenkins-genomel']
-            int randNum = new Random().nextInt(namespaces.length);
+            int nsIndex = 3; // force genomel to test it ... new Random().nextInt(namespaces.length);
             uid = env.service+"-"+env.quaySuffix+"-"+env.BUILD_NUMBER
             int lockStatus = 1;
 
             // try to find an unlocked namespace
             for (int i=0; i < namespaces.length && lockStatus != 0; ++i) {
-              randNum = (randNum + i) % namespaces.length;
-              env.KUBECTL_NAMESPACE = namespaces[randNum]
+              nsIndex = (nsIndex + i) % namespaces.length;
+              env.KUBECTL_NAMESPACE = namespaces[nsIndex]
               println "selected namespace $env.KUBECTL_NAMESPACE on executor $env.EXECUTOR_NUMBER"
               println "attempting to lock namespace $env.KUBECTL_NAMESPACE with a wait time of 1 minutes"
               withEnv(['GEN3_NOPROXY=true', "GEN3_HOME=$env.WORKSPACE/cloud-automation"]) {
