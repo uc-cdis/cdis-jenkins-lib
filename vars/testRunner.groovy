88a81570a9250b5ba5017672445c2afc2d9b0ad8
Ted Summer <ted.summer2@gmail.com>
2019-01-11 09:45:44 -0600

    feat(testRunner): fetch data client

--- vars/testRunner.groovy
+++ vars/testRunner.groovy
@@ -55,6 +55,41 @@ def simulateData(String namespace) {
   }
 }
 
+/**
+* Fetches data client
+*/
+def fetchDataClient() {
+  dir('dataclient') {
+    // we get the data client from master, unless the service being
+    // tested is the data client itself, in which case we get the
+    // executable for the current branch
+    // Note: the data client does not use Jenkins yet (see PXP-2211)
+    branch = "master"
+    if (this.config.currentRepoName == "cdis-data-client") {
+      branch = env.CHANGE_BRANCH
+      println "Testing cdis-data-client on branch " + branch
+    }
+
+      // Note: at this time, tests are always run on linux
+    os = "linux"
+
+      // download the gen3 data client executable from S3
+    download_location = "dataclient.zip"
+    sh String.format("aws s3 cp s3://cdis-dc-builds/%s/dataclient_%s.zip %s", branch, os, download_location)
+    assert fileExists(download_location)
+    unzip(download_location)
+
+      // make sure we can execute it
+    executable_name = "gen3-client"
+    assert fileExists(executable_name)
+    sh "mv $executable_name $env.WORKSPACE/$executable_name"
+    sh "chmod u+x $env.WORKSPACE/$executable_name"
+    sh "$env.WORKSPACE/$executable_name --version"
+
+    println "Data client successfully set up at: $env.WORKSPACE/$executable_name"
+  }
+}
+
 def teardown() {
   if (this.startedIntegrationTests) {
     junit "gen3-qa/output/*.xml"
