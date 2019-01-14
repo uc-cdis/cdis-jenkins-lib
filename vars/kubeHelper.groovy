005320bdb002a2d7be965490ada5a0e0b9f600d2
Ted Summer <ted.summer2@gmail.com>
2019-01-14 10:27:26 -0600

    debug(kubeHelper): echo env vars before commands

--- vars/kubeHelper.groovy
+++ vars/kubeHelper.groovy
@@ -44,7 +44,12 @@ def setCloudAutomationPath(String path) {
 */
 def kube(Closure body) {
   withEnv(['GEN3_NOPROXY=true', "vpc_name=${this.vpcName}", "GEN3_HOME=${this.cloudAutomationPath}", "KUBECTL_NAMESPACE=${this.kubectlNamespace}"]) {
-    echo "  GEN3_HOME is ${env.GEN3_HOME}\n  KUBECTL_NAMESPACE is ${env.KUBECTL_NAMESPACE}"
+    echo "GEN3_HOME is $env.GEN3_HOME"
+    echo "CHANGE_BRANCH is $env.CHANGE_BRANCH"
+    echo "GIT_COMMIT is $env.GIT_COMMIT"
+    echo "KUBECTL_NAMESPACE is $env.KUBECTL_NAMESPACE"
+    echo "WORKSPACE is $env.WORKSPACE"
+    echo "vpc_name is $env.vpc_name"
     return body()
   }
 }
