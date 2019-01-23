08e325bb2847ba5c2c8ae3c50602c2a9d4a97d0b
Ted Summer <ted.summer2@gmail.com>
2019-01-23 14:03:51 -0600

    refactor(testHelper): helper for running test and sim data

--- vars/gitHelper.groovy
+++ vars/gitHelper.groovy
@@ -3,7 +3,7 @@
 */
 def fetchAllRepos(String currentRepoName) {
   // checkout current branch to set environment variables
-  checkout(scm)
+  checkout scm;
   sh("env")
   dir('gen3-qa') {
     if (currentRepoName == "gen3-qa") {
