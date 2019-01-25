205335e3306e4c897683a6ba6955897fa195ddd1
Ted Summer <ted.summer2@gmail.com>
2019-01-25 09:59:27 -0600

    refactor(testHelper): helper for running test and sim data

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -2,12 +2,16 @@
 
 // This is the main entry point for all testing
 def call(body) {
+  println("run commit $env.GIT_COMMIT")
+  println("run prev: $env.GIT_PREVIOUS_COMMIT")
   // evaluate the body block, and collect configuration into the object
   def config = [:]
   body.resolveStrategy = Closure.DELEGATE_FIRST
   body.delegate = config
   body()
 
+  println("run commit $env.GIT_COMMIT")
+  println("run prev: $env.GIT_PREVIOUS_COMMIT")
   // call the pipeline indicated in config
   if (config.pipeline == 'microservice') {
     microservicePipeline(config)
