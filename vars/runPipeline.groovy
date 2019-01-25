951011ad3e27680df9c14862c99c1f8aba632761
Ted Summer <ted.summer2@gmail.com>
2019-01-25 10:14:12 -0600

    refactor(testHelper): helper for running test and sim data

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -4,6 +4,7 @@
 def call(body) {
   println("run commit $env.GIT_COMMIT")
   println("run prev: $env.GIT_PREVIOUS_COMMIT")
+  println(scm)
   // evaluate the body block, and collect configuration into the object
   def config = [:]
   body.resolveStrategy = Closure.DELEGATE_FIRST
