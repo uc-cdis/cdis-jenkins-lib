df08df4c99cea3920694ffd45b8da7a070b21243
Ted Summer <ted.summer2@gmail.com>
2019-01-25 11:23:12 -0600

    refactor(testHelper): helper for running test and sim data

--- vars/gitHelper.groovy
+++ vars/gitHelper.groovy
@@ -25,7 +25,7 @@ def setGitEnvVars(String currentRepoName) {
 * Pulls common repositories used for testing
 */
 def fetchAllRepos(String currentRepoName) {
-  setGitEnvVars()
+  setGitEnvVars(currentRepoName)
   dir('gen3-qa') {
     if (currentRepoName == "gen3-qa") {
       // testing the gen3-qa repo - check out the test branch here
