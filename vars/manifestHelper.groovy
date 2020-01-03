e7d02d414a312d2a70ccb6e60de3af07b652bb18
Reuben Pasquini <reuben@frickjack.com>
2020-01-03 15:22:30 -0600

    fix(manifestHelper): do not copy manifests/
    hatchery-config cannot just be copied - need
    to change namespaces, etc

--- vars/manifestHelper.groovy
+++ vars/manifestHelper.groovy
@@ -55,9 +55,6 @@ def mergeManifest(String changedDir, String selectedNamespace) {
 */
 def overwriteConfigFolders(String changedDir, String selectedNamespace) {
     List<String> folders = sh(returnStdout: true, script: "ls tmpGitClone/$changedDir").split()
-    if (folders.contains('manifests')) {
-      sh(script: "cp -rf tmpGitClone/$changedDir/manifests cdis-manifest/${selectedNamespace}.planx-pla.net/")
-    }
     if (folders.contains('portal')) {
       sh(script: "cp -rf tmpGitClone/$changedDir/portal cdis-manifest/${selectedNamespace}.planx-pla.net/")
 
