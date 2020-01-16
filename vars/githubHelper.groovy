1731107264a2e9ebccb0de3dbebbf91e31e13b41
Marcelo Costa <marceloc@uchicago.edu>
2020-01-16 08:45:50 -0600

    checking repo name properly

--- vars/githubHelper.groovy
+++ vars/githubHelper.groovy
@@ -2,9 +2,9 @@ import groovy.json.JsonSlurperClassic
 def fetchLabels() {
   try{
     def PR_NUMBER = env.BRANCH_NAME.split('-')[1];
-    def REPO_NAME = env.JOB_NAME;
+    def REPO_NAME = env.JOB_NAME.split('/')[1];
     println('REPO_NAME: ' + REPO_NAME);
-    labels_url="https://api.github.com/repos/uc-cdis/gen3-qa/issues/${PR_NUMBER}/labels"
+    labels_url="https://api.github.com/repos/uc-cdis/${REPO_NAME}/issues/${PR_NUMBER}/labels"
     println("Shooting a request to: " + labels_url);
     def get = new URL(labels_url).openConnection();
     def getRC = get.getResponseCode();
