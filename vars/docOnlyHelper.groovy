774c60e16e3cbfb0cfa410641d02ddd088a5c832
Marcelo Costa <marceloc@uchicago.edu>
2020-03-10 23:59:15 -0500

    remove githooks from git diff to avoid manifest lookup

--- vars/docOnlyHelper.groovy
+++ vars/docOnlyHelper.groovy
@@ -3,14 +3,11 @@ def checkTestSkippingCriteria() {
   for (String key : fileChanges.keySet()) {
     fileChange = fileChanges[key][0]
     def releasesFolder = fileChange =~ /^(releases\/.*)/
-    def gitHookScript = fileChange =~ /^(.githooks\/.*)/
     def textFile = fileChange =~ /(.*\.md)|(.*\.txt)|(.*\.feature)/
     if (releasesFolder) {
       println('Found releases folder: ' + releasesFolder[0][0])
     } else if (textFile) {
       println('Found text file: ' + textFile[0][0])
-    } else if (githook) {
-      println('Found githook script: ' + gitHookScript[0][0])
     } else {
       println('This PR is eligible for testing')
       return false
