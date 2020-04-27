80546bd3811f53285e01eda4e2ec5c85db4b3448
Marcelo R Costa <themarcelor@gmail.com>
2020-04-27 09:44:17 -0500

    chore(doc) skip tests for OpenAPI Swagger docs (#78)
    * chore(doc) skip tests for OpenAPI Swagger docs
    
    * escape dot
    
    Co-authored-by: Marcelo Costa <marceloc@uchicago.edu>

--- vars/docOnlyHelper.groovy
+++ vars/docOnlyHelper.groovy
@@ -3,9 +3,12 @@ def checkTestSkippingCriteria() {
   for (String key : fileChanges.keySet()) {
     fileChange = fileChanges[key][0]
     def releasesFolder = fileChange =~ /^(releases\/.*)/
+    def openapisFolder = fileChange =~ /^(openapis\/*\.yaml)/
     def docFile = fileChange =~ /(.*\.md)|(.*\.png)|(.*\.txt)|(.*\.feature)/
     if (releasesFolder) {
       println('Found releases folder: ' + releasesFolder[0][0])
+    } else if (openapisFolder) {
+      println('Found yaml file: ' + openapisFolder[0][0])
     } else if (docFile) {
       println('Found text file: ' + docFile[0][0])
     } else {
