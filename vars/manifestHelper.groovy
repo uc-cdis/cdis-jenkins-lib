1893eda5b6b38ce2ebd28754f904f49a00a0eaae
Marcelo Costa <marceloc@uchicago.edu>
2020-05-21 21:13:50 -0500

    trying to re-inject sower block from the target manifest

--- vars/manifestHelper.groovy
+++ vars/manifestHelper.groovy
@@ -26,7 +26,7 @@ def editService(String commonsHostname, String serviceName, String quayBranchNam
 def mergeManifest(String changedDir, String selectedNamespace) {
   String od = sh(returnStdout: true, script: "jq -r .global.dictionary_url < tmpGitClone/$changedDir/manifest.json").trim()
   String pa = sh(returnStdout: true, script: "jq -r .global.portal_app < tmpGitClone/$changedDir/manifest.json").trim()
-  String sj = sh(returnStdout: true, script: "jq -r .sower < tmpGitClone/$changedDir/manifest.json").trim()
+  String sj = sh(returnStdout: true, script: "jq .sower < tmpGitClone/$changedDir/manifest.json").trim()
   println(sj);
   String s = sh(returnStdout: true, script: "jq -r keys < cdis-manifest/${selectedNamespace}.planx-pla.net/manifest.json")
   println s
@@ -45,7 +45,7 @@ def mergeManifest(String changedDir, String selectedNamespace) {
   }
   sh(returnStdout: true, script: "bs=\$(jq -r .versions < tmpGitClone/$changedDir/manifest.json) "
           + "&& old=\$(cat cdis-manifest/${selectedNamespace}.planx-pla.net/manifest.json) "
-          + """&& echo \$old | jq -r --arg od ${od} --arg pa ${pa} --argjson sj \"\$sj\" --argjson vs \"\$bs\"""" 
+          + """&& echo \$old | jq -r --arg od ${od} --arg pa ${pa} --argjson sj ${sj} --argjson vs \"\$bs\"""" 
           + / '(.global.dictionary_url) |=/ + "\$od" + / | (.global.portal_app) |=/ + "\$pa"
           + / | (.versions) |=/ + "\$vs" + / | (.sower) |=/ + "\$sj" + /'/ + " > cdis-manifest/${selectedNamespace}.planx-pla.net/manifest.json")
   String rs = sh(returnStdout: true, script: "cat cdis-manifest/${selectedNamespace}.planx-pla.net/manifest.json")
