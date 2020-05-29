ff2d671f30bc2f3775afea7bbd9499947e014349
Marcelo Costa <marceloc@uchicago.edu>
2020-05-29 14:00:15 -0500

    tweak declarative syntax

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -132,161 +132,173 @@ spec:
 	  }
 	}
       }
-      if (pipeConfig.MANIFEST == null || pipeConfig.MANIFEST == false || pipeConfig.MANIFEST != "True") {
-	// Setup stages for NON manifest builds
-	stage('WaitForQuayBuild') {
-	  steps {
-	    script {
-	      try {
-	        if(!doNotRunTests) {
-	          quayHelper.waitForBuild(
-	       	pipeConfig['quayRegistry'],
-	       	pipeConfig['currentBranchFormatted']
-	          )
-	        } else {
-	          Utils.markStageSkippedForConditional(STAGE_NAME)
-	        }
-	      } catch (e) {
-	        pipelineHelper.handleError(e)
+      stage('WaitForQuayBuild') {
+        steps {
+	  script {
+	    try {
+	      if(!doNotRunTests) {
+	        if (pipeConfig.MANIFEST == null || pipeConfig.MANIFEST == false || pipeConfig.MANIFEST != "True") {
+		  // for NON manifest builds
+                  quayHelper.waitForBuild(
+                    pipeConfig['quayRegistry'],
+                    pipeConfig['currentBranchFormatted']
+                  )
+		} else {
+                  Utils.markStageSkippedForConditional(STAGE_NAME)
+                }
+	      } else {
+	        Utils.markStageSkippedForConditional(STAGE_NAME)
 	      }
+	    } catch (e) {
+	      pipelineHelper.handleError(e)
 	    }
-	  }
-	}
-	stage('SelectNamespace') {
-	  try {
-	    if(!doNotRunTests) {
-	      (kubectlNamespace, lock) = kubeHelper.selectAndLockNamespace(pipeConfig['UID'], namespaces)
-	      kubeLocks << lock
-	    } else {
-	      Utils.markStageSkippedForConditional(STAGE_NAME)
-	    }
-	  } catch (e) {
-	    pipelineHelper.handleError(e)
-	  }
-	}
-	stage('ModifyManifest') {
-	  try {
-	    if(!doNotRunTests && !doNotModifyManifest) {
-	      manifestHelper.editService(
-		kubeHelper.getHostname(kubectlNamespace),
-		pipeConfig.serviceTesting.name,
-		pipeConfig.serviceTesting.branch
-	      )
-	    } else {
-	      Utils.markStageSkippedForConditional(STAGE_NAME)
-	    }
-	  } catch (e) {
-	    pipelineHelper.handleError(e)
 	  }
 	}
       }
-      if (pipeConfig.MANIFEST != null && (pipeConfig.MANIFEST == true || pipeConfig.MANIFEST == "True")) {
-	// Setup stages for MANIFEST builds
-	stage('SelectNamespace') {
-	  try {
-	    if(!doNotRunTests) {
-	      (kubectlNamespace, lock) = kubeHelper.selectAndLockNamespace(pipeConfig['UID'], namespaces)
-	      kubeLocks << lock
-	    } else {
-	      Utils.markStageSkippedForConditional(STAGE_NAME)
+      stage('SelectNamespace') {
+	steps {
+	  script {
+	    try {
+	      if(!doNotRunTests) {
+	        (kubectlNamespace, lock) = kubeHelper.selectAndLockNamespace(pipeConfig['UID'], namespaces)
+		kubeLocks << lock
+	      } else {
+	        Utils.markStageSkippedForConditional(STAGE_NAME)
+	      }
+	    } catch (e) {
+	      pipelineHelper.handleError(e)
 	    }
-	  } catch (e) {
-	    pipelineHelper.handleError(e)
 	  }
-	}
-	stage('ModifyManifest') {
-	  try {
-	    if(!doNotRunTests) {
-	      testedEnv = manifestHelper.manifestDiff(kubectlNamespace)
-	    } else {
-	      Utils.markStageSkippedForConditional(STAGE_NAME)
-	    }
-	  } catch (e) {
-	    pipelineHelper.handleError(e)
+        }
+      }
+      stage('ModifyManifest') {
+        steps {
+          script {
+            try {
+              if(!doNotRunTests && !doNotModifyManifest) {
+                if (pipeConfig.MANIFEST == null || pipeConfig.MANIFEST == false || pipeConfig.MANIFEST != "True") {
+                  manifestHelper.editService(
+                    kubeHelper.getHostname(kubectlNamespace),
+                    pipeConfig.serviceTesting.name,
+                    pipeConfig.serviceTesting.branch
+                  )
+                } else {
+                  testedEnv = manifestHelper.manifestDiff(kubectlNamespace)
+                }
+              } else {
+                Utils.markStageSkippedForConditional(STAGE_NAME)
+              }
+            } catch (e) {
+              pipelineHelper.handleError(e)
+            }
 	  }
 	}
       }
       stage('K8sReset') {
-	  try {
-	  if(!doNotRunTests) {
-	    // adding the reset-lock lock in case reset fails before unlocking
-	    kubeLocks << kubeHelper.newKubeLock(kubectlNamespace, "gen3-reset", "reset-lock")
-	    kubeHelper.reset(kubectlNamespace)
-	  } else {
-	    Utils.markStageSkippedForConditional(STAGE_NAME)
+        steps {
+	  script {
+            try {
+              if(!doNotRunTests) {
+                // adding the reset-lock lock in case reset fails before unlocking
+                kubeLocks << kubeHelper.newKubeLock(kubectlNamespace, "gen3-reset", "reset-lock")
+                kubeHelper.reset(kubectlNamespace)
+              } else {
+                Utils.markStageSkippedForConditional(STAGE_NAME)
+              }
+            } catch (e) {
+              pipelineHelper.handleError(e)
+            }
 	  }
-	} catch (e) {
-	  pipelineHelper.handleError(e)
 	}
       }
       stage('VerifyClusterHealth') {
-	  try {
-	  if(!doNotRunTests) {
-	    kubeHelper.waitForPods(kubectlNamespace)
-	    testHelper.checkPodHealth(kubectlNamespace, testedEnv)
-	  } else {
-	    Utils.markStageSkippedForConditional(STAGE_NAME)
+        steps {
+	  script {
+            try {
+	      if(!doNotRunTests) {
+	        kubeHelper.waitForPods(kubectlNamespace)
+	        testHelper.checkPodHealth(kubectlNamespace, testedEnv)
+	      } else {
+	        Utils.markStageSkippedForConditional(STAGE_NAME)
+	      }
+            } catch (e) {
+	      pipelineHelper.handleError(e)
+	    }
 	  }
-	} catch (e) {
-	  pipelineHelper.handleError(e)
 	}
       }
       stage('GenerateData') {
-	try {
-	  if(!doNotRunTests) {
-	    testHelper.simulateData(kubectlNamespace)
-	  } else {
-	    Utils.markStageSkippedForConditional(STAGE_NAME)
+        steps {
+	  script {
+	    try {
+	      if(!doNotRunTests) {
+	        testHelper.simulateData(kubectlNamespace)
+	      } else {
+	        Utils.markStageSkippedForConditional(STAGE_NAME)
+	      }
+            } catch (e) {
+	      pipelineHelper.handleError(e)
+	    }
 	  }
-	} catch (e) {
-	  pipelineHelper.handleError(e)
 	}
       }
       stage('FetchDataClient') {
-	try {
-	  if(!doNotRunTests) {
-	    // we get the data client from master, unless the service being
-	    // tested is the data client itself, in which case we get the
-	    // executable for the current branch
-	    dataCliBranch = "master"
-	    if (pipeConfig.currentRepoName == "cdis-data-client") {
-	      dataCliBranch = env.CHANGE_BRANCH
+        steps {
+	  script {
+            try {
+	      if(!doNotRunTests) {
+	        // we get the data client from master, unless the service being
+	        // tested is the data client itself, in which case we get the
+	        // executable for the current branch
+	        dataCliBranch = "master"
+	        if (pipeConfig.currentRepoName == "cdis-data-client") {
+	          dataCliBranch = env.CHANGE_BRANCH
+	        }
+	        testHelper.fetchDataClient(dataCliBranch)
+	      } else {
+	        Utils.markStageSkippedForConditional(STAGE_NAME)
+	      }
+	    } catch (e) {
+	      pipelineHelper.handleError(e)
 	    }
-	    testHelper.fetchDataClient(dataCliBranch)
-	  } else {
-	    Utils.markStageSkippedForConditional(STAGE_NAME)
 	  }
-	} catch (e) {
-	  pipelineHelper.handleError(e)
 	}
       }
       stage('RunTests') {
-	try {
-	  if(!doNotRunTests) {
-	    testHelper.runIntegrationTests(
-	      kubectlNamespace,
-	      pipeConfig.serviceTesting.name,
-	      testedEnv,
-	      isGen3Release,
-	      selectedTest
-	    )
-	  } else {
-	    Utils.markStageSkippedForConditional(STAGE_NAME)
+        steps {
+	  script {
+	    try {
+	      if(!doNotRunTests) {
+	        testHelper.runIntegrationTests(
+	          kubectlNamespace,
+	          pipeConfig.serviceTesting.name,
+	          testedEnv,
+	          isGen3Release,
+	          selectedTest
+	        )
+	      } else {
+	        Utils.markStageSkippedForConditional(STAGE_NAME)
+	      }
+            } catch (e) {
+	      pipelineHelper.handleError(e)
+	    }
 	  }
-	} catch (e) {
-	  pipelineHelper.handleError(e)
 	}
       }
       stage('CleanS3') {
-	try {
-	  if(!doNotRunTests) {
-	    testHelper.cleanS3()
-	  } else {
-	    Utils.markStageSkippedForConditional(STAGE_NAME)
-	  }
-	} catch (e) {
-	  pipelineHelper.handleError(e)
-	}
+        steps {
+          script {
+            try {
+	      if(!doNotRunTests) {
+	        testHelper.cleanS3()
+	      } else {
+	        Utils.markStageSkippedForConditional(STAGE_NAME)
+	      }
+	    } catch (e) {
+	      pipelineHelper.handleError(e)
+	    }
+          }
+        }
       }
     }
     post {
