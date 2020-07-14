a0f6d28d8e2467cf8d2a1df43c6d7a4e87314599
Marcelo R Costa <themarcelor@gmail.com>
2020-07-14 15:46:23 -0500

    chore(qa): Abort PR check if the PR is marked as a draft (#101)
    * chore(qa): Abort Jenkins job if PR is marked as a draft
    
    * verify condition against a boolean
    
    Co-authored-by: Marcelo Costa <marceloc@uchicago.edu>

--- vars/githubHelper.groovy
+++ vars/githubHelper.groovy
@@ -22,3 +22,27 @@ def fetchLabels() {
   }
   return null;
 }
+
+def isDraft() {
+  try{
+    def PR_NUMBER = env.BRANCH_NAME.split('-')[1];
+    def REPO_NAME = env.JOB_NAME.split('/')[1];
+    println('REPO_NAME: ' + REPO_NAME);
+    pr_url="https://api.github.com/repos/uc-cdis/${REPO_NAME}/pulls/${PR_NUMBER}"
+    println("Shooting a request to: " + pr_url);
+    def get = new URL(pr_url).openConnection();
+    def getRC = get.getResponseCode();
+    println(getRC);
+    def jsonSlurper = new groovy.json.JsonSlurperClassic();
+    prMetadata = null;
+    if(getRC.equals(200)) {
+      prMetadataJson = get.getInputStream().getText();
+      println(prMetadataJson);
+      prMetadata = jsonSlurper.parseText(prMetadataJson);
+    }
+    return prMetadata['draft'];
+  } catch (e) {
+    pipelineHelper.handleError(e)
+  }
+  return null;
+}
