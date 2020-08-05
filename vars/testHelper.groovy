7dd7893a59883841881f761b2b01cced3097a5d3
Marcelo Costa <marceloc@uchicago.edu>
2020-08-05 01:13:38 -0500

    fix syntax for WithCredentials block

--- vars/testHelper.groovy
+++ vars/testHelper.groovy
@@ -35,10 +35,10 @@ def gen3Qa(String namespace, Closure body, List<String> add_env_variables = []) 
 * @param testedEnv - environment the test is being run for (for manifest PRs)
 */
 def runIntegrationTests(String namespace, String service, String testedEnv, String isGen3Release,  List<String> selectedTests = ['all']) {
-  withCredentials(
-    [usernamePassword(credentialsId: 'ras-test-user1-for-ci-tests', usernameVariable: 'RAS_TEST_USER_1_USERNAME', passwordVariable: 'RAS_TEST_USER_1_PASSWORD')],
-    [usernamePassword(credentialsId: 'ras-test-user2-for-ci-tests', usernameVariable: 'RAS_TEST_USER_2_USERNAME', passwordVariable: 'RAS_TEST_USER_2_PASSWORD')]
-  ) {
+  withCredentials([
+    usernamePassword(credentialsId: 'ras-test-user1-for-ci-tests', usernameVariable: 'RAS_TEST_USER_1_USERNAME', passwordVariable: 'RAS_TEST_USER_1_PASSWORD'),
+    usernamePassword(credentialsId: 'ras-test-user2-for-ci-tests', usernameVariable: 'RAS_TEST_USER_2_USERNAME', passwordVariable: 'RAS_TEST_USER_2_PASSWORD')
+  ]) {
     dir('gen3-qa') {
       gen3Qa(namespace, {
         // clean up old test artifacts in the workspace
