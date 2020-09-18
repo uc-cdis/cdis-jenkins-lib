4f925bc1dac523f552337abe18de287bf454bf07
Marcelo R Costa <themarcelor@gmail.com>
2020-09-18 10:55:22 -0500

    chore(qa): Ignore missing testsuite xmls (#114)
    * chore(qa): Ignore missing testsuite xmls
    
    * tweak string interpolation for logging
    
    Co-authored-by: Marcelo Costa <marceloc@uchicago.edu>

--- vars/xmlHelper.groovy
+++ vars/xmlHelper.groovy
@@ -39,34 +39,40 @@ def assembleFeatureLabelMap(failedTestSuites) {
 
 def identifyFailedTestSuites() {
   List<String> failedTestSuites = [];
-  def xmlTestSuiteFilesRaw = sh(returnStdout: true, script: "ls output/*-testsuite.xml")
-  def xmlTestSuiteFiles = xmlTestSuiteFilesRaw.split('\n')
-  println(xmlTestSuiteFiles)
+  try {
+    def xmlTestSuiteFilesRaw = sh(returnStdout: true, script: "ls output/*-testsuite.xml")
+    def xmlTestSuiteFiles = xmlTestSuiteFilesRaw.split('\n')
+    println(xmlTestSuiteFiles)
 
-  xmlTestSuiteFiles.each{ xmlFile->
-    println('reading test suite file... ' + xmlFile)
+    xmlTestSuiteFiles.each{ xmlFile->
+      println('reading test suite file... ' + xmlFile)
                   
-    def testSuiteName = sh(
-      returnStdout: true,
-      script: "python -c \"import lxml.etree; print(lxml.etree.parse(\\\"${xmlFile}\\\").xpath('//ns2:test-suite/name/text()', namespaces={'ns2': 'urn:model.allure.qatools.yandex.ru'})[0].split(':')[0])\""
-    )
+      def testSuiteName = sh(
+        returnStdout: true,
+        script: "python -c \"import lxml.etree; print(lxml.etree.parse(\\\"${xmlFile}\\\").xpath('//ns2:test-suite/name/text()', namespaces={'ns2': 'urn:model.allure.qatools.yandex.ru'})[0].split(':')[0])\""
+      )
 
-    println(testSuiteName)
+      println(testSuiteName)
 
-    def testSuiteResultsRaw = sh(
-      returnStdout: true,
-      script: "python -c \"import lxml.etree; print(','.join(lxml.etree.parse(\\\"${xmlFile}\\\").xpath('//ns2:test-suite/test-cases/test-case/@status', namespaces={'ns2': 'urn:model.allure.qatools.yandex.ru'})))\""
-    )
+      def testSuiteResultsRaw = sh(
+        returnStdout: true,
+        script: "python -c \"import lxml.etree; print(','.join(lxml.etree.parse(\\\"${xmlFile}\\\").xpath('//ns2:test-suite/test-cases/test-case/@status', namespaces={'ns2': 'urn:model.allure.qatools.yandex.ru'})))\""
+      )
 
-    println('### ##' + StringUtils.chomp(testSuiteResultsRaw).split(","))
+      println('### ##' + StringUtils.chomp(testSuiteResultsRaw).split(","))
 
-    def testSuiteResults = StringUtils.chomp(testSuiteResultsRaw).split(",")
-    println(testSuiteResults)
+      def testSuiteResults = StringUtils.chomp(testSuiteResultsRaw).split(",")
+      println(testSuiteResults)
                   
-    if ('failed' in testSuiteResults) {
-      failedTestSuites.add( StringUtils.chomp(testSuiteName) )
+      if ('failed' in testSuiteResults) {
+        failedTestSuites.add( StringUtils.chomp(testSuiteName) )
+      }
     }
+    println("full list of failedTestSuites: ${failedTestSuites}")
+  } catch (e) {
+    println("Something wrong happened: ${e}")
+    println("Ignore and return empty list")
+    return [];
   }
-  println("full list of failedTestSuites: ${failedTestSuites}")
   return failedTestSuites
 }
