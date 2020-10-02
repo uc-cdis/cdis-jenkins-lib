184984f06c7dd11092f0fb1ac00fa50522b66efd
Marcelo Costa <marceloc@uchicago.edu>
2020-10-02 14:11:17 -0500

    replace env. var.

--- vars/kubeHelper.groovy
+++ vars/kubeHelper.groovy
@@ -18,7 +18,7 @@ def kube(String kubectlNamespace, Closure body) {
     echo "GIT_COMMIT is $env.GIT_COMMIT"
     echo "KUBECTL_NAMESPACE is $env.KUBECTL_NAMESPACE"
     echo "WORKSPACE is $env.WORKSPACE"
-    def sanitized_workspace = $env.WORKSPACE.replaceAll(" ", "\\\\ ");
+    env.WORKSPACE = $env.WORKSPACE.replaceAll(" ", "\\\\ ");
     echo "sanitized WORKSPACE is $env.WORKSPACE"
     echo "vpc_name is $env.vpc_name"
     return body()
