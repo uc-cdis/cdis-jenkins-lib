f6d9087cbcde71f91c49b4c90e06f9abb8660751
Marcelo Costa <marceloc@uchicago.edu>
2020-10-02 15:06:24 -0500

    sanitize the workspace env. var. outside function blocks

--- vars/kubeHelper.groovy
+++ vars/kubeHelper.groovy
@@ -1,3 +1,9 @@
+echo "WORKSPACE is $env.WORKSPACE"
+if (env.WORKSPACE.indexOf("\\") == -1) {
+  env.WORKSPACE = env.WORKSPACE.replaceAll(" ", "\\\\ ");
+  echo "sanitized WORKSPACE is $env.WORKSPACE"
+}
+
 def cloudAutomationPath() {
   return "${env.WORKSPACE}/cloud-automation"
 }
@@ -18,10 +24,6 @@ def kube(String kubectlNamespace, Closure body) {
     echo "GIT_COMMIT is $env.GIT_COMMIT"
     echo "KUBECTL_NAMESPACE is $env.KUBECTL_NAMESPACE"
     echo "WORKSPACE is $env.WORKSPACE"
-    if (env.WORKSPACE.indexOf("\\") == -1) {
-      env.WORKSPACE = env.WORKSPACE.replaceAll(" ", "\\\\ ");
-      echo "sanitized WORKSPACE is $env.WORKSPACE"
-    }
     echo "vpc_name is $env.vpc_name"
     return body()
   }
