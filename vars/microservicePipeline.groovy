98a58528f1ae4801641a9c914761677c1988975f
Marcelo Costa <marceloc@uchicago.edu>
2020-10-13 17:42:42 -0500

    skip inside helper

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -86,7 +86,7 @@ def call(Map config) {
       if (pipeConfig.MANIFEST == null || pipeConfig.MANIFEST == false || pipeConfig.MANIFEST != "True") {
         // Setup stages for NON manifest builds
         stage('WaitForQuayBuild') {
-          if(!doNotRunTests && pipeConfig['quayRegistry'] != "jenkins-lib") {
+          if(!doNotRunTests) {
             quayHelper.waitForBuild(
               pipeConfig['quayRegistry'],
               pipeConfig['currentBranchFormatted']
