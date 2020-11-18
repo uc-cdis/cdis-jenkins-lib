71476b9426b44ea2e52eecd3eb68c1db088cf077
Marcelo R Costa <themarcelor@gmail.com>
2020-11-18 11:30:18 -0600

    Chore/tweak data simulation based on tested env (#132)
    * chore(qa): tweak data simulation based on testedEnv
    
    * chore(qa): tweak data simulation based on testedEnv
    
    Co-authored-by: Marcelo Costa <marceloc@uchicago.edu>

--- vars/testHelper.groovy
+++ vars/testHelper.groovy
@@ -86,10 +86,10 @@ def runIntegrationTests(String namespace, String service, String testedEnv, Stri
 *
 * @param namespace - namespace to simulate data for
 */
-def simulateData(String namespace) {
+def simulateData(String namespace, String testedEnv="") {
   dir('gen3-qa') {
     gen3Qa(namespace, {
-      sh "bash ./jenkins-simulate-data.sh ${namespace}"
+      sh "bash ./jenkins-simulate-data.sh ${namespace} ${testedEnv}"
     })
   }
 }
