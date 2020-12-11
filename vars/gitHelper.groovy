e457864cd7f964b5b3385eff304560f07f58abaf
Marcelo Costa <marceloc@uchicago.edu>
2020-12-10 18:55:03 -0600

    move gitops-qa checkout to a point after the folder cleanup

--- vars/gitHelper.groovy
+++ vars/gitHelper.groovy
@@ -67,6 +67,8 @@ def fetchAllRepos(String currentRepoName) {
     }
   }
   dir('tmpGitClone') {
+    checkout(scm: scm, clearWorkspace: true)
+    
     if (currentRepoName.contains("dictionary")) {
       // keep copy of gitops-qa to extract and transform jenkins environments manifests
       println("INFO: checkout gitops-qa for a DICTIONARY repo PR ...")
@@ -75,7 +77,6 @@ def fetchAllRepos(String currentRepoName) {
         branch: 'master'
       )
     }
-    checkout(scm: scm, clearWorkspace: true)
   }
 
   setGitEnvVars(currentRepoName)
