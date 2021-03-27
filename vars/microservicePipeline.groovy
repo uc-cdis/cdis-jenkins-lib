72e8fff0433ec478e4474edfb9631eba7de2a1a1
Marcelo Costa <marceloc@uchicago.edu>
2021-03-27 16:17:10 -0500

    skip k8s reset

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -207,7 +207,7 @@ def call(Map config) {
          metricsHelper.writeMetricWithResult(STAGE_NAME, true)
 	}
       }
-
+/*
       stage('K8sReset') {
        try {
         if(!doNotRunTests) {
@@ -223,7 +223,7 @@ def call(Map config) {
        }
        metricsHelper.writeMetricWithResult(STAGE_NAME, true)
       }
-
+*/
       stage('VerifyClusterHealth') {
        try {
         if(!doNotRunTests) {
