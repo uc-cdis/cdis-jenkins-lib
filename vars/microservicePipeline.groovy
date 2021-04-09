92275103b0390f48473be240ef7ecf2d0296f7b9
Marcelo Costa <marceloc@uchicago.edu>
2021-04-09 12:13:06 -0500

    disable k8s reset

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -211,6 +211,7 @@ def call(Map config) {
          metricsHelper.writeMetricWithResult(STAGE_NAME, true)
 	}
       }
+/*
       stage('K8sReset') {
        try {
         if(!doNotRunTests) {
@@ -273,6 +274,7 @@ def call(Map config) {
        }
        metricsHelper.writeMetricWithResult(STAGE_NAME, true)
       }
+*/
       if(!runParallelTests) {
         stage('RunTests') {
           try {
