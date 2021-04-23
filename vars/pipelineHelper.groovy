e9ed97444174624e8b7add9fc0c70b1013c1c1d4
Marcelo Costa <themarcelor@gmail.com>
2021-04-23 14:11:42 -0500

    fix(k8s-debugging): Drop logs on root folder of the PR workspace

--- vars/pipelineHelper.groovy
+++ vars/pipelineHelper.groovy
@@ -82,7 +82,7 @@ def handleError(e) {
 def teardown(String buildResult) {
   archiveArtifacts(artifacts: '**/output/*.png', allowEmptyArchive: true)
   archiveArtifacts(artifacts: '**/output/*.log.gz', allowEmptyArchive: true)
-  archiveArtifacts(artifacts: '**/output/*.log', allowEmptyArchive: true)
+  archiveArtifacts(artifacts: '*.log', allowEmptyArchive: true)
 
   if ("UNSTABLE" == buildResult) {
     echo "Build Unstable!"
