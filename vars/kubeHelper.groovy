772c262a7fff697c108f9e084e25dc0c3c43781e
Marcelo Costa <themarcelor@gmail.com>
2021-05-03 19:01:01 -0500

    fix jenkins blueocean pipeline build detail URL

--- vars/kubeHelper.groovy
+++ vars/kubeHelper.groovy
@@ -104,7 +104,7 @@ def sendSlackNotification(String kubectlNamespace, String isNightlyBuild = "fals
       // include url in the notification (e.g., # https://jenkins.planx-pla.net/blue/organizations/jenkins/CDIS_GitHub_Org%2Fcdis-manifest/detail/PR-2503/7/pipeline)
       def PR_NUMBER = env.BRANCH_NAME.split('-')[1];
       def REPO_NAME = env.JOB_NAME.split('/')[1];
-      slackSend(color: 'bad', channel: isNightlyBuild == "true" ? "#nightly-builds" : "#gen3-qa-notifications", message: "OH NOUS! :open_mouth: K8sReset failed :rotating_light: on ${kubectlNamespace}. Go check the failing pods / logs here: https://jenkins.planx-pla.net/blue/organizations/jenkins/CDIS_GitHub_Org%2F${REPO_NAME}/detail/${PR_NUMBER}/${env.BUILD_NUMBER}/pipeline .")
+      slackSend(color: 'bad', channel: isNightlyBuild == "true" ? "#nightly-builds" : "#gen3-qa-notifications", message: "OH NOUS! :open_mouth: K8sReset failed :rotating_light: on ${kubectlNamespace}. Go check the failing pods / logs here: https://jenkins.planx-pla.net/blue/organizations/jenkins/CDIS_GitHub_Org%2F${REPO_NAME}/detail/PR-${PR_NUMBER}/${env.BUILD_NUMBER}/pipeline .")
     }
   )
 }
