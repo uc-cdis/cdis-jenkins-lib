de69650d9f11e062bbd59a4a8123f368e1be9f97
jingh8 <jingh8@uchicago.edu>
2021-05-12 13:49:35 -0500

    add toList()

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -328,7 +328,7 @@ def call(Map config) {
                 println("### ## selectedTests: ${listOfAllTestSuites}")
                 // Too many test suites cause a ArrayIndexOutOfBoundsException error
                 // Removing items to identify the number of tests that Jenkins can accommodate
-                selectedTests = new ArrayList<String>(listOfAllTestSuites.subList(0, 40));
+                selectedTests = new ArrayList<String>(listOfAllTestSuites.toList().subList(0, 40));
               }
               env.GEN3_SKIP_PROJ_SETUP = "true"
             } else {
