6ce1dff7158cfb34cb387332bf17ab7a82853e97
jingh8 <jingh8@uchicago.edu>
2021-06-23 19:06:06 -0500

    fix change single value

--- vars/manifestHelper.groovy
+++ vars/manifestHelper.groovy
@@ -122,9 +122,9 @@ def mergeManifest(String changedDir, String selectedNamespace) {
   }
   // replace netpolicy
   sh(returnStdout: true, script: "if [ -f \"netpolicy.json\" ]; then "
-    + "old=\$(cat cdis-manifest/${selectedNamespace}.planx-pla.net/manifest.json) && echo \$old | jq -r --argjson sp \"\$(cat netpolicy.json)\" '(.global.netpolicy) = \$sp' > cdis-manifest/${selectedNamespace}.planx-pla.net/manifest.json; "
+    + "old=\$(cat cdis-manifest/${selectedNamespace}.planx-pla.net/manifest.json) && echo \$old | jq --arg sp \"\$(cat netpolicy.json)\" '.global.netpolicy = \$sp' > cdis-manifest/${selectedNamespace}.planx-pla.net/manifest.json; "
     + "else "
-    + "jq 'del(.global.netpolicy)' cdis-manifest/${selectedNamespace}.planx-pla.net/manifest.json > manifest_tmp.json && mv manifest_tmp.json cdis-manifest/${selectedNamespace}.planx-pla.net/manifest.json; "
+    + "old=\$(cat cdis-manifest/${selectedNamespace}.planx-pla.net/manifest.json) && echo \$old | jq -r 'del(.global.netpolicy)' > cdis-manifest/${selectedNamespace}.planx-pla.net/manifest.json;"
     + "fi")
   // replace Portal block
   sh(returnStdout: true, script: "if [ -f \"portal_block.json\" ]; then "
