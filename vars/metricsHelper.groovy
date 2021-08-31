c5a8c61a91cd5f447129141631250a9891242a5a
Jing Huang <71466688+jingh8@users.noreply.github.com>
2021-08-31 15:27:25 -0500

    fix/don't fail jenkins job if the influxdb crash (#218)

--- vars/metricsHelper.groovy
+++ vars/metricsHelper.groovy
@@ -3,7 +3,7 @@ def writeMetricWithResult(stageName, isSuccess) {
   def REPO_NAME = env.JOB_NAME.split('/')[1];
   def PR_NUMBER = env.BRANCH_NAME.split('-')[1];
   
-  def output = sh(script: "curl -i -XPOST \"http://influxdb:8086/write?db=ci_metrics\" --data-binary \"${measure},stage_name=${stageName},repo_name=${REPO_NAME},pr_num=${PR_NUMBER} ${measure}=1\"", returnStdout: true);
+  def output = sh(script: "curl -i -XPOST \"http://influxdb:8086/write?db=ci_metrics\" --data-binary \"${measure},stage_name=${stageName},repo_name=${REPO_NAME},pr_num=${PR_NUMBER} ${measure}=1\" || exit 0", returnStdout: true);
 
   println(output)
 }
