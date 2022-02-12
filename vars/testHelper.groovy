f028bd12107bdcac9307df74a61a728b708d4dc3
Hara Prasad <haraprasadj@uchicago.edu>
2022-02-12 08:26:09 -0800

    add DD keys to CI runs
    to capture metrics using dogapi

--- vars/testHelper.groovy
+++ vars/testHelper.groovy
@@ -38,7 +38,9 @@ def soonToBeLegacyRunIntegrationTests(String namespace, String service, String t
   withCredentials([
     usernamePassword(credentialsId: 'ras-test-user1-for-ci-tests', usernameVariable: 'RAS_TEST_USER_1_USERNAME', passwordVariable: 'RAS_TEST_USER_1_PASSWORD'),
     usernamePassword(credentialsId: 'ras-test-user2-for-ci-tests', usernameVariable: 'RAS_TEST_USER_2_USERNAME', passwordVariable: 'RAS_TEST_USER_2_PASSWORD'),
-    usernamePassword(credentialsId: 'jenkins-user-api-token', usernameVariable: 'JENKINS_USERNAME', passwordVariable: 'JENKINS_USER_API_TOKEN')
+    usernamePassword(credentialsId: 'jenkins-user-api-token', usernameVariable: 'JENKINS_USERNAME', passwordVariable: 'JENKINS_USER_API_TOKEN'),
+    string(credentialsId: 'DD_API_KEY', variable: 'SECRET'),
+    string(credentialsId: 'DD_API_KEY', variable: 'SECRET')
   ]) {
     dir('gen3-qa') {
       gen3Qa(namespace, {
