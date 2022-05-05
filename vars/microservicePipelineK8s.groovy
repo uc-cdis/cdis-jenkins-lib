ad9d767fd3ecc6989fba0104c64ac001e5db3ded
John McCann <johnmccann@uchicago.edu>
2022-05-05 17:08:58 -0500

    fix(nightly-build): grep portal/gitops.json

--- vars/microservicePipelineK8s.groovy
+++ vars/microservicePipelineK8s.groovy
@@ -164,7 +164,7 @@ spec:
                                 if (isNightlyBuild == "true") {
                                     exportToPFBFeatureEnabledExitCode = sh(
                                         returnStdout: true,
-                                        script: "grep '\"export-to-pfb\"' tmpGitClone/nightly.planx-pla.net/manifest.json"
+                                        script: "grep '\"export-to-pfb\"' tmpGitClone/nightly.planx-pla.net/portal/gitops.json"
                                     ) as Integer
                                     if (exportToPFBFeatureEnabledExitCode == 0) {
                                         selectedTests.add("suites/portal/pfbExportTest.js")
