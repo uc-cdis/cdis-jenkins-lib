c76d4e5ca9ad4a9a52477b6d8076ab149b42eb45
Atharva Rane <41084525+atharvar28@users.noreply.github.com>
2022-05-17 17:14:17 -0400

    Update gitHelper.groovy

--- vars/gitHelper.groovy
+++ vars/gitHelper.groovy
@@ -25,7 +25,7 @@ def fetchAllRepos(String currentRepoName) {
     } else {
       git(
         url: 'https://github.com/uc-cdis/gen3-qa.git',
-        branch: 'master'
+        branch: 'fix/studyViewer_tests'
       );
     }
   }
@@ -37,7 +37,7 @@ def fetchAllRepos(String currentRepoName) {
     } else {
       git(
         url: 'https://github.com/uc-cdis/data-simulator.git',
-        branch: 'fix/studyViewer_tests'
+        branch: 'master'
       );
     }
   }
