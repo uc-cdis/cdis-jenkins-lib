e3680b23002c1e1638053bc0156e7f8512716587
Jing Huang <71466688+jingh8@users.noreply.github.com>
2022-05-18 11:38:55 -0500

    Update gitHelper.groovy

--- vars/gitHelper.groovy
+++ vars/gitHelper.groovy
@@ -62,7 +62,7 @@ def fetchAllRepos(String currentRepoName) {
     } else {
       git(
         url: 'https://github.com/uc-cdis/cloud-automation.git',
-        branch: 'master'
+        branch: 'chore/add-argo-and-cohort-middleware-to-roll-all'
       );
     }
   }
