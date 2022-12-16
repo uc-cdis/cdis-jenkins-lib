676d7db41cb6dfa9be141cb2e799096e1a2e48b8
Hara Prasad <haraprasadj@uchicago.edu>
2022-12-16 09:59:30 -0800

    separate envs for releases and services

--- vars/ciEnvsHelper.groovy
+++ vars/ciEnvsHelper.groovy
@@ -1,8 +1,8 @@
 def fetchCIEnvs(runOnGen3CIWorker = false) {
   try{
-    def jenkins_envs_url="https://cdistest-public-test-bucket.s3.amazonaws.com/jenkins-envs-services.txt";
-    if (runOnGen3CIWorker) {
-      jenkins_envs_url="https://cdistest-public-test-bucket.s3.amazonaws.com/jenkins-envs-releases.txt";
+    def jenkins_envs_url="https://cdistest-public-test-bucket.s3.amazonaws.com/jenkins-envs-releases.txt";
+    if (pipeConfig.MANIFEST == null || pipeConfig.MANIFEST == false || pipeConfig.MANIFEST != "True") {
+      jenkins_envs_url="https://cdistest-public-test-bucket.s3.amazonaws.com/jenkins-envs-services.txt";
     }
     println("Shooting a request to: " + jenkins_envs_url);
     def get = new URL(jenkins_envs_url).openConnection();
