b95a1414e6b8e5037dd51749187a1418402ddd68
Atharva Rane <41084525+atharvar28@users.noreply.github.com>
2023-10-12 13:34:36 -0400

    Update gitHelper.groovy

--- vars/gitHelper.groovy
+++ vars/gitHelper.groovy
@@ -62,7 +62,7 @@ def fetchAllRepos(String currentRepoName) {
     } else {
       git(
         url: 'https://github.com/uc-cdis/cloud-automation.git',
-        branch: 'fix/skip_setup_ci_runs_cohort_middleware'
+        branch: 'master'
       );
     }
   }
