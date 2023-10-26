9eea3c9d062443fbbc7d1d4b0d88e84defd34168
Pauline Ribeyre <4224001+paulineribeyre@users.noreply.github.com>
2023-10-26 10:46:21 -0500

    Update microservicePipeline.groovy

--- vars/microservicePipeline.groovy
+++ vars/microservicePipeline.groovy
@@ -95,7 +95,7 @@ spec:
         memory: 500Mi
         ephemeral-storage: 500Mi
   - name: shell
-    image: quay.io/cdis/gen3-ci-worker:master
+    image: quay.io/cdis/gen3-ci-worker:fix-sim-py3.9
     imagePullPolicy: Always
     command:
     - sleep
