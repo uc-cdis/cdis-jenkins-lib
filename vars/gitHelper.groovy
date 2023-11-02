f8cb195a7f6362a43df9c4659f063c750b833978
EliseCastle23 <109446148+EliseCastle23@users.noreply.github.com>
2023-11-02 09:20:32 -0600

    Update gitHelper.groovy
    moving back to branch "master"

--- vars/gitHelper.groovy
+++ vars/gitHelper.groovy
@@ -62,7 +62,7 @@ def fetchAllRepos(String currentRepoName) {
     } else {
       git(
         url: 'https://github.com/uc-cdis/cloud-automation.git',
-        branch: 'EliseCastle23-patch2'
+        branch: 'master'
       );
     }
   }
