a2b4fe81f3cd04d3d5f9e8a5a8eb4366ba5aab05
Hara Prasad Juvvala <haraprasadj@uchicago.edu>
2023-12-23 02:41:34 -0800

    debug

--- vars/pipelineHelper.groovy
+++ vars/pipelineHelper.groovy
@@ -80,6 +80,7 @@ def handleError(e) {
 * @param buildResult - the current build result (accessible by currentBuild.result)
 */
 def teardown(String buildResult) {
+  sh(script: "find / -name pipeline.log -type d")
   sh(script: "sed -i 's/\\*//g' pipeline.log")
   archiveArtifacts(artifacts: '**/output/*.png', allowEmptyArchive: true)
   archiveArtifacts(artifacts: '**/output/*.log', allowEmptyArchive: true)
