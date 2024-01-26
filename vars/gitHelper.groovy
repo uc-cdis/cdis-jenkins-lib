eb7731c74b13a1d590dad09126682ace04b88373
EliseCastle23 <109446148+EliseCastle23@users.noreply.github.com>
2024-01-26 16:39:28 -0700

    Update gitHelper.groovy

--- vars/gitHelper.groovy
+++ vars/gitHelper.groovy
@@ -62,7 +62,7 @@ def fetchAllRepos(String currentRepoName) {
     } else {
       git(
         url: 'https://github.com/uc-cdis/cloud-automation.git',
-        branch: 'master'
+        branch: 'feat_GPE-1173'
       );
     }
   }
